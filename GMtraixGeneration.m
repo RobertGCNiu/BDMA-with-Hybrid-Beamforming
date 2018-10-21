aecbdb2e361b17627760bcd7435aa52e2c34e537
robert <714984847@qq.com>
2018-10-21 22:28:01 +0800

    add select users

---
+++ GMtraixGeneration.m
@@ -0,0 +1,21 @@
+function [maxsir,p] = GMtraixGeneration(T)
+N_users = size(T,1);
+G = zeros(N_users, N_users);
+for u =1:N_users
+    G(u,:) = T(u, :)/T(u,u);
+end
+
+[power, lamda]=eig(G);
+eigvalue = diag(lamda);
+sir = 1./(eigvalue-1);
+sir(imag(sir)~=0) = -10000;
+%  for u = 1:N_users
+    [maxsir,maxindex] = max(real(sir));
+    p = power(:,maxindex);
+%     if sum(p>=0)==N_users || sum(p<=0)==N_users
+%         break
+%     else
+%         sir(maxindex) =-100000;
+%     end
+% end
+end
