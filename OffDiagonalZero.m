33cfe4b95aa1f2de8dbe14f2a9d0232266dee68d
robert <714984847@qq.com>
2018-10-23 22:58:38 +0800

    niu change

--- OffDiagonalZero.m
+++ OffDiagonalZero.m
@@ -1,6 +1,7 @@
-function F = OffDiagonalZero(N_RF, N_users);
+function F_odz = OffDiagonalZero(N_RF, N_users,F_full)
 
-for 
+F_Blockpart = blkdiag(ones(N_RF),ones(N_users-N_RF));
+F_odz=F_full.* F_Blockpart;
 
 end
 
